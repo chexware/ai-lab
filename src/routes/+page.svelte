717f20b9bb234e000a6a3fc99032ca4e0b9f741b
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2024-07-10 23:43:33 +0100

    feat: added initial interface for text and image generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import { Textarea } from "$lib/components/ui/textarea";
+  //import { Textarea } from "$lib/components/ui/textarea";
   import * as Tabs from "$lib/components/ui/tabs";
-  //import * as Card from "$lib/components/ui/card/index";
+  //import * as Form from "$lib/components/ui/form";
+  //import { Input } from "$lib/components/ui/input";
+  import * as Card from "$lib/components/ui/card/index";
+  import { Button } from "$lib/components/ui/button/index";
 </script>
 
 
@@ -12,13 +15,29 @@
       <Tabs.Trigger value="image-generation">Image Generation</Tabs.Trigger>
    </Tabs.List>
    <Tabs.Content value="computer-vision" class="pt-3 bg-gray-light rounded-sm h-[600px] text-center">
-      Make changes to your account here 1.
+      Hello
    </Tabs.Content>
    <Tabs.Content value="text-generation" class="pt-3 bg-gray-light rounded-sm h-[600px] text-center">
-      Make changes to your account here 2.
+      <Card.Root class="mx-3 bg-blue rounded-lg h-[580px] text-center">
+         <Card.Header>
+           <Card.Title>Story Generator</Card.Title>
+           <Card.Description>Generating a story based on an image description</Card.Description>
+         </Card.Header>
+         <Card.Content>
+            <Button>Generate Story</Button>
+         </Card.Content>
+       </Card.Root>
    </Tabs.Content>
    <Tabs.Content value="image-generation" class="pt-3 bg-gray-light rounded-sm h-[600px] text-center">
-      Make changes to your account here 3.
+      <Card.Root class="mx-3 bg-blue rounded-lg h-[580px] text-center">
+         <Card.Header>
+           <Card.Title>Image Generator</Card.Title>
+           <Card.Description>Generating image based on a given story</Card.Description>
+         </Card.Header>
+         <Card.Content>
+            <Button>Generate Image</Button>
+         </Card.Content>
+      </Card.Root>
    </Tabs.Content>
 </Tabs.Root>    
 
@@ -28,7 +47,6 @@
      background-color: theme(colors.gray-dark);
      color: theme(colors.gray);
    }
-   
 </style>
 
 
