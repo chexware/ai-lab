e73c07f196baa234375ca87c592e2099ee39e77b
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2024-07-22 00:24:18 +0100

    feat: inital image description tab implementation

---
+++ src/lib/utils/handlers.ts
@@ -0,0 +1,3 @@
+export function processImageUpload(event: any, imageFile: any) {
+   
+  }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,55 @@
 <script lang="ts">
-  //import { Textarea } from "$lib/components/ui/textarea";
-  import * as Tabs from "$lib/components/ui/tabs";
-  //import * as Form from "$lib/components/ui/form";
-  //import { Input } from "$lib/components/ui/input";
-  import * as Card from "$lib/components/ui/card/index";
-  import { Button } from "$lib/components/ui/button/index";
+  import { onMount } from 'svelte';
+  import { Textarea } from '$lib/components/ui/textarea';
+  import * as Tabs from '$lib/components/ui/tabs';
+  //import * as Form from '$lib/components/ui/form';
+  import { Label } from '$lib/components/ui/label';
+  import { Input } from '$lib/components/ui/input';
+  import * as Card from '$lib/components/ui/card/index';
+  import { Button } from '$lib/components/ui/button/index';
+  import OpenAI from 'openai';
+
+
+  const authorizedFileExtensions = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'];
+  
+  // State Bindings
+  let imageFile = '';
+  let description = '';
+  let isLoading = false;
+ 
+  function handleImageUpload(event: any) {
+   const file = event.target.files[0];
+   if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+         const base64Data = reader.result;
+         if (base64Data) imageFile = base64Data.toString();
+      };
+      reader.readAsDataURL(file);
+   }
+  }
+
+  const handleTextGeneration = async () => {
+   isLoading = true;
+   const response = await fetch('http://127.0.0.1:7860/interrogator/prompt',{
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+         image: imageFile,
+         clip_model_name: 'ViT-L-14/openai',
+         mode: 'fast',
+      }),
+   });
+   const data = await response.json();
+   description = data.prompt;
+   isLoading = false;
+  }
+ 
+  $: imageFile
+  $: description
+  $: isLoading
 </script>
 
 
@@ -15,7 +60,26 @@
       <Tabs.Trigger value="image-generation">Image Generation</Tabs.Trigger>
    </Tabs.List>
    <Tabs.Content value="computer-vision" class="pt-3 bg-gray-light rounded-sm h-[600px] text-center">
-      Hello
+      <Card.Root class="mx-3 bg-blue rounded-lg h-[580px] text-center">
+         <Card.Header>
+           <Card.Title>Image Processing</Card.Title>
+           <Card.Description>Generating a story based on an image description</Card.Description>
+         </Card.Header>
+         <Card.Content >
+            <div class="flex w-1/2 items-center space-x-2">
+               <Label for="file">Select Image</Label>
+               <Input type="file" accept={authorizedFileExtensions.join(',')} id="file" name="file" on:change={handleImageUpload}/> 
+            </div>
+            {#if imageFile }
+               <img src={imageFile} alt="Uploaded Image" class="pt-10"/>
+               <Button class="mt-5" on:click={handleTextGeneration}>Generate Description</Button> 
+            {/if}
+            {#if description && !isLoading }
+               <Label for="image-description">Descritpion: </Label>
+               <Textarea disabled id="image-description" placeholder={description} />
+            {/if}
+         </Card.Content>
+       </Card.Root>
    </Tabs.Content>
    <Tabs.Content value="text-generation" class="pt-3 bg-gray-light rounded-sm h-[600px] text-center">
       <Card.Root class="mx-3 bg-blue rounded-lg h-[580px] text-center">
@@ -23,8 +87,8 @@
            <Card.Title>Story Generator</Card.Title>
            <Card.Description>Generating a story based on an image description</Card.Description>
          </Card.Header>
-         <Card.Content>
-            <Button>Generate Story</Button>
+         <Card.Content >
+            <Button>Generate Story</Button> 
          </Card.Content>
        </Card.Root>
    </Tabs.Content>
